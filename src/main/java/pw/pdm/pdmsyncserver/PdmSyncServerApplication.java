d2d44c7501e9686307e30de501270ee5a66610e5
mikeyang <18604713262@163.com>
2024-06-30 23:57:09 -0400

    Initial

---
+++ src/main/java/pw/pdm/pdmsyncserver/PdmSyncServerApplication.java
@@ -0,0 +1,13 @@
+package pw.pdm.pdmsyncserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdmSyncServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdmSyncServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pw/pdm/pdmsyncserver/PdmSyncServerApplicationTests.java
@@ -0,0 +1,13 @@
+package pw.pdm.pdmsyncserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdmSyncServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
